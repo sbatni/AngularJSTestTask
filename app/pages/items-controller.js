157f198ef4deedf28de92079a52785dee53901b1
sbatni <shubha.batni@harmonicinc.com>
2015-07-08 14:50:17 -0700

    Fixed code based on code review comments

--- app/pages/items-controller.js
+++ app/pages/items-controller.js
@@ -4,42 +4,37 @@
 
 var app = angular.module('pmpApp');
 
-app.controller('ItemListCtrl', function($scope) {
-    this.items = [];
-    this.prod={};
-    this.selectedIndex=-1;
+app.controller('ItemListCtrl', function() {
+  this.items = [];
+  this.prod = {};
+  this.selectedIndex = -1;
 
-    this.saveItem = function () {
-        if(this.selectedIndex === -1)
-        {
-            this.items.push({
-                'name': this.prod.name,
-                'sku': this.prod.sku,
-                'price': this.prod.price
-            });
-        }
-        else {
-            this.items[this.selectedIndex] = this.prod;
-        }
-        this.prod = {};
-        this.selectedIndex = -1;
-        $scope.inputForm.$setPristine();
-    };
+  this.saveItem = function(form) {
+    if (this.selectedIndex === -1) {
+      this.items.push({
+        'name': this.prod.name,
+        'sku': this.prod.sku,
+        'price': this.prod.price
+      });
+    } else {
+      this.items[this.selectedIndex] = this.prod;
+    }
+    this.prod = {};
+    this.selectedIndex = -1;
+    if (form) {
+      form.$setPristine();
+    }
+  };
 
-    this.deleteItem = function (idx) {
-        if(confirm('Delete selected product?')) {
-            this.items.splice(idx, 1);
-        }
-    };
+  this.deleteItem = function(idx) {
+    if (confirm('Delete selected product?')) {
+      this.items.splice(idx, 1);
+    }
+  };
 
-    this.editItem = function (idx) {
-        this.prod = {};
-        this.selectedIndex = idx;
-        var name = this.items[idx].name;
-        var sku = this.items[idx].sku;
-        var price = this.items[idx].price;
-        this.prod = {'name': name,
-                    'sku':  sku,
-                    'price':  price};
-    };
+  this.editItem = function(idx) {
+    this.prod = {};
+    this.selectedIndex = idx;
+    angular.extend(this.prod, this.items[idx])
+  };
 });
--- app/pages/unique-validator.js
+++ app/pages/unique-validator.js
@@ -6,28 +6,30 @@
 var app = angular.module('pmpApp');
 
 app.directive('uniqueValidater', function() {
-    return {
-        restrict: 'A',
-        scope: {
-            items: '=',
-            index: '='
-        },
-        require: 'ngModel',
+  return {
+    restrict: 'A',
+    scope: {
+      items: '=',
+      index: '='
+    },
+    require: 'ngModel',
 
-        link: function(scope, elem, attr, ctrl) {
+    link: function(scope, elem, attr, ctrl) {
 
-            ctrl.$parsers.unshift(function(value) {
-                var isValid = true;
-                for (var key = 0, size = scope.items.length; key < size; key++) {
-                    if (scope.items[key][ctrl.$name] === value && key !== scope.index) {
-                        isValid = false;
-                        break;
-                    }
-                }
-                ctrl.$setValidity('unique', isValid);
-
-                return isValid ? value : undefined;
-            });
+      ctrl.$parsers.unshift(function(value) {
+        var isValid = true;
+        for (var key = 0, size = scope.items.length; key < size; key++) {
+          if (scope.items[key][ctrl.$name] === value && key !== scope.index) {
+            isValid = false;
+            break;
+          }
         }
-    };
+        ctrl.$setValidity('uniqness', isValid);
+
+        return isValid ? value : undefined;
+      });
+    }
+  };
 });
+
+
